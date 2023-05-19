9c48f253182748768e0f3dd934e5bec500ae3416
axumrs <rolson867@gmail.com>
2023-05-19 14:05:22 +0800

    文章列表

--- pages/topic/index.jsx
+++ pages/topic/index.jsx
@@ -6,132 +6,47 @@ import TopicListItem from "../../components/TopicListItem";
 import Paginate from "../../components/Paginate";
 import { get } from "../../fetcher/fetcher";
 
-export default function TopicIndex() {
+export default function TopicIndex({ topicPaginate }) {
+  const { data: topicList } = topicPaginate;
   return (
     <>
       <PageMeta>文章</PageMeta>
       <PageTitle>文章</PageTitle>
+      {/* <div>{JSON.stringify(topicPaginate)}</div> */}
 
       <Card>
         <ul className="flex flex-col divide-y">
-          <li className="py-2">
-            <TopicListItem
-              subject={{
-                slug: "1",
-                name: "全栈：AXUM、React、NextJS 和 TailWindCSS",
-              }}
-              topic={{
-                slug: "1",
-                title: "React: SEO挑战、服务端渲染及本地存储",
-                summary:
-                  "经过紧张的学习，React课程终于暂告一个段落了。本章将是一个相对轻松的内容，我们一起探讨一下React 应用的 SEO 以及为什么需要服务端渲染，同时对 React课程做个简单的小结。",
-                cover: "",
-                tags: ["标签1", "标签2"],
-                try_readable: false,
-              }}
-            />
-          </li>
-
-          <li className="py-2">
-            <TopicListItem
-              subject={{
-                slug: "2",
-                name: "全栈：AXUM、React、NextJS 和 TailWindCSS",
-              }}
-              topic={{
-                slug: "2",
-                title: "React: SEO挑战、服务端渲染及本地存储",
-                summary:
-                  "经过紧张的学习，React课程终于暂告一个段落了。本章将是一个相对轻松的内容，我们一起探讨一下React 应用的 SEO 以及为什么需要服务端渲染，同时对 React课程做个简单的小结。",
-                cover:
-                  "https://cdn.hashnode.com/res/hashnode/image/upload/v1684170814296/50fd7774-8684-45b1-ab70-6e5f82707b3f.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
-                tags: ["标签1", "标签2"],
-                try_readable: true,
-              }}
-            />
-          </li>
-          <li className="py-2">
-            <TopicListItem
-              subject={{
-                slug: "3",
-                name: "全栈：AXUM、React、NextJS 和 TailWindCSS",
-              }}
-              topic={{
-                slug: "3",
-                title: "React: SEO挑战、服务端渲染及本地存储",
-                summary:
-                  "经过紧张的学习，React课程终于暂告一个段落了。本章将是一个相对轻松的内容，我们一起探讨一下React 应用的 SEO 以及为什么需要服务端渲染，同时对 React课程做个简单的小结。",
-                cover: "",
-                tags: ["标签1", "标签2"],
-                try_readable: true,
-              }}
-            />
-          </li>
-          <li className="py-2">
-            <TopicListItem
-              subject={{
-                slug: "4",
-                name: "全栈：AXUM、React、NextJS 和 TailWindCSS",
-              }}
-              topic={{
-                slug: "4",
-                title: "React: SEO挑战、服务端渲染及本地存储",
-                summary:
-                  "经过紧张的学习，React课程终于暂告一个段落了。本章将是一个相对轻松的内容，我们一起探讨一下React 应用的 SEO 以及为什么需要服务端渲染，同时对 React课程做个简单的小结。",
-                cover: "",
-                tags: ["标签1", "标签2"],
-                try_readable: false,
-              }}
-            />
-          </li>
-
-          <li className="py-2">
-            <TopicListItem
-              subject={{
-                slug: "5",
-                name: "全栈：AXUM、React、NextJS 和 TailWindCSS",
-              }}
-              topic={{
-                slug: "5",
-                title: "React: SEO挑战、服务端渲染及本地存储",
-                summary:
-                  "经过紧张的学习，React课程终于暂告一个段落了。本章将是一个相对轻松的内容，我们一起探讨一下React 应用的 SEO 以及为什么需要服务端渲染，同时对 React课程做个简单的小结。",
-                cover:
-                  "https://cdn.hashnode.com/res/hashnode/image/upload/v1684170814296/50fd7774-8684-45b1-ab70-6e5f82707b3f.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
-                tags: ["标签1", "标签2"],
-                try_readable: false,
-              }}
-            />
-          </li>
-          <li className="py-2">
-            <TopicListItem
-              subject={{
-                slug: "6",
-                name: "全栈：AXUM、React、NextJS 和 TailWindCSS",
-              }}
-              topic={{
-                slug: "6",
-                title: "React: SEO挑战、服务端渲染及本地存储",
-                summary:
-                  "经过紧张的学习，React课程终于暂告一个段落了。本章将是一个相对轻松的内容，我们一起探讨一下React 应用的 SEO 以及为什么需要服务端渲染，同时对 React课程做个简单的小结。",
-                cover: "",
-                tags: ["标签1", "标签2"],
-                try_readable: true,
-              }}
-            />
-          </li>
+          {topicList.map((t) => (
+            <li className="py-2">
+              <TopicListItem
+                subject={{
+                  slug: t.subject_slug,
+                  name: t.subject_name,
+                }}
+                topic={{ ...t, tags: t.tag_names.split(",") }}
+              />
+            </li>
+          ))}
         </ul>
       </Card>
 
       <div className="my-3 text-right">
-        <Paginate />
+        <Paginate
+          page={topicPaginate.page}
+          totalPage={topicPaginate.total_page}
+          baseUrl="/topic"
+        />
       </div>
     </>
   );
 }
 
-// export async function getServerSideProps() {
-//   const { data: subjectList } = await get("/subject");
+export async function getServerSideProps({ query }) {
+  const { page: pageStr } = query;
+  const page = parseInt(pageStr) || 0;
+  const { data: topicPaginate } = await get(
+    `/topic?page=${page}&page_size=30&is_del=false`
+  );
 
-//   return { props: { subjectList } };
-// }
+  return { props: { topicPaginate } };
+}
