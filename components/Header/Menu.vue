beb41267f50f92e98df9489ae003642b94ceaa13
axumrs <rolson867@gmail.com>
2024-07-08 15:56:35 +0800

    主体

--- components/Header/Menu.vue
+++ components/Header/Menu.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-  <ul class="flex justify-end items-center gap-x-2">
+  <ul class="flex justify-end items-center gap-x-0 lg:gap-x-4">
     <li>
       <NuxtLink
-        class="block w-full h-full border rounded-full px-3 py-1 text-white/95 border-blue-700 bg-blue-600 lg:text-current lg:border-gray-200 lg:bg-transparent lg:hover:bg-gray-50"
+        class="block w-full h-full border rounded-2xl px-2 py-0.5 text-sm text-white/95 border-blue-700 bg-blue-600 lg:text-current lg:border-gray-200 lg:bg-transparent lg:hover:bg-gray-50 lg:px-3 lg:py-1 lg:text-base lg:rounded-full"
         >登录</NuxtLink
       >
     </li>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,14 @@
 
 <template>
   <Header />
-  <section>
-    <slot></slot>
-  </section>
+  <Container class="grid grid-cols-12 items-start gap-x-4 my-6">
+    <aside
+      class="fixed left-0 top-0 w-3/4 lg:col-start-1 lg:col-span-2 bg-red-50"
+    >
+      a
+    </aside>
+    <main class="col-span-12 lg:col-start-3 lg:col-span-10 bg-green-50">
+      <slot></slot>
+    </main>
+  </Container>
 </template>
