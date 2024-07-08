ac2c4d869d7c02792dc56c03ecf887a23afe030c
axumrs <rolson867@gmail.com>
2024-07-08 13:48:40 +0800

    init

---
+++ app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+useSeoMeta({ title: "AXUM中文网 - 带你使用 axum 构建企业应用 - axum.rs" });
+</script>
+
+<template>
+  <NuxtLayout>
+    <NuxtPage :key="useRoute().fullPath" />
+  </NuxtLayout>
+</template>
