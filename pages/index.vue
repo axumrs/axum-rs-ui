dda4eaa084f5908d6f52e299afcad1239f42b09e
axumrs <rolson867@gmail.com>
2024-09-26 10:46:06 +0800

    文章列表

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <HomeBox title="专题列表" href="/" more-text="全部专题">
+  <HomeBox title="专题列表" href="/subject" more-text="全部专题">
     <SubjectTop />
   </HomeBox>
 
-  <HomeBox title="热门文章" href="/" more-text="全部文章" class="my-3">
+  <HomeBox title="热门文章" href="/topic" more-text="全部文章" class="my-3">
     <TopicList />
   </HomeBox>
 </template>
---
+++ pages/topic/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { page: pageParam } = useRoute().query;
+const page = computed(() => parseInt(pageParam?.toString() || "0", 10) || 0);
+</script>
+
+<template>
+  <PageTitle title="文章" />
+
+  <TopicList class="my-3" />
+
+  <Pagination
+    class="justify-end my-3"
+    :p="{ page, page_size: 30, total: 301, total_page: 11 }"
+  />
+</template>
