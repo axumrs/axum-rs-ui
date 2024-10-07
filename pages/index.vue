92f988015f9aa903cb02d10876cca1f36090cde2
axumrs <rolson867@gmail.com>
2024-10-07 10:46:02 +0800

    文章

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@ const { $get } = use$fetch();
 const { $isLoading } = use$status();
 
 const topSubjectList = ref<Subject[]>([]);
+const topTopicList = ref<TopicWithSubjectAndTags[]>([]);
 const loadTopSubjectList = async () => {
   $get<Subject[]>(
     "/user/subject/top",
@@ -15,9 +16,17 @@ const loadTopSubjectList = async () => {
   );
 };
 
+const loadTopTopicList = async () => {
+  $get<TopicWithSubjectAndTags[]>("/user/topic/top", (v) => {
+    if (v) {
+      topTopicList.value = v;
+    }
+  });
+};
+
 const loadData = () => {
   $isLoading.value = true;
-  Promise.all([loadTopSubjectList()])
+  Promise.all([loadTopSubjectList(), loadTopTopicList()])
     .then()
     .finally(() => {
       $isLoading.value = false;
@@ -33,10 +42,14 @@ loadData();
       v-if="topSubjectList && topSubjectList.length > 0"
       :list="topSubjectList"
     />
-    <div v-else>LOADING</div>
   </HomeBox>
 
   <HomeBox title="热门文章" href="/topic" more-text="全部文章" class="my-3">
-    <TopicList />
+    <TopicList
+      :topic-list="topTopicList"
+      :item-with-border="true"
+      v-if="topTopicList && topTopicList.length > 0"
+      class="space-y-4"
+    />
   </HomeBox>
 </template>
--- pages/topic/index.vue
+++ pages/topic/index.vue
@@ -1,15 +1,36 @@
 <script setup lang="ts">
 const { page: pageParam } = useRoute().query;
 const page = computed(() => parseInt(pageParam?.toString() || "0", 10) || 0);
+
+const pm = ref<PaginationMeta>();
+const topicList = ref<TopicWithSubjectAndTags[]>([]);
+
+const { $get } = use$fetch();
+
+const loadData = async () => {
+  await $get<Pagination<TopicWithSubjectAndTags>>(
+    "/user/topic",
+    (v) => {
+      if (v) {
+        topicList.value = v.data || [];
+        pm.value = v;
+      }
+    },
+    { query: { page: page.value } }
+  );
+};
+
+await loadData();
 </script>
 
 <template>
   <PageTitle title="文章" icon="heroicons:document-duplicate" />
 
-  <TopicList class="my-4" />
-
-  <Pagination
-    class="justify-end my-3"
-    :p="{ page, page_size: 30, total: 301, total_page: 11 }"
+  <TopicList
+    class="my-4 space-y-4"
+    :item-with-border="true"
+    :topic-list="topicList"
   />
+
+  <Pagination class="justify-end my-3" :p="pm" v-if="pm" />
 </template>
