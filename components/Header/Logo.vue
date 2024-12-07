13cf97c1f21ea124df7849930b11347ffa32ec05
axumrs <rolson867@gmail.com>
2024-12-07 14:37:25 +0800

    logo & favicon

--- components/Header/Logo.vue
+++ components/Header/Logo.vue
@@ -5,7 +5,10 @@
     class="flex justify-start items-center gap-x-2 lg:static lg:max-w-max lg:z-auto lg:top-auto"
     to="/"
   >
-    <img src="https://file.axum.rs/asset/logo.png" class="w-6 object-cover" />
+    <img
+      src="https://file.axum.eu.org/asset/logo.png"
+      class="w-6 object-cover"
+    />
     <h2 class="text-lg lg:text-xl">AXUM中文网</h2>
   </NuxtLink>
 </template>
